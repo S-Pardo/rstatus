57cc6539f5e0e1a7dad0d5a246b5a05606cd0d79
Sebastian Pardo <sapardosuazo@gmail.com>
2020-08-10 23:21:33 -0400

    implementation for generic scripts

--- src/feature.rs
+++ src/feature.rs
@@ -1,4 +1,17 @@
+use std::process::Command;
+
 pub struct Generic {
-    path: String,
-    data: String,
+    command: Command,
+}
+
+impl Generic {
+    pub fn new(path: String) -> Self {
+        Generic { command: Command::new(path) }
+    }
+
+    pub fn execute(&mut self) -> String {
+        let output = self.command.output().unwrap();
+        let mut data = String::from_utf8(output.stdout).unwrap();
+        data.trim().to_string()
+    }
 }
